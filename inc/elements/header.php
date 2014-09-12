cf5fbc1d4026fa1d61f8140eb8e1f794202b1377
gfmr90 <gfmr90@gmail.com>
2014-09-12 16:38:29 +1000

    Latestupdate

---
+++ inc/elements/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+	<head>
+
+		<title>Signup</title>
+		<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+		<meta http-equiv="Content-Style-Type" content="text/css" />
+		<meta http-equiv="imagetoolbar" content="no" />
+		<meta name="keywords" content="" />
+		<meta name="description" content="" />
+		<meta name="author" content="" />
+		<meta name="copyright" content="" />
+
+		<link href="inc/css/style.css" rel="stylesheet" type="text/css" />
+		<link href="inc/css/zurb_buttons.css" rel="stylesheet" type="text/css" />
+
+	</head>
+
+	<body>
+
+		<div id="content">
+		
+		<h1>Signup Today!</h1>
+		
---
+++ inc/php/config.php
@@ -0,0 +1,5 @@
+<?php
+
+//connect to the database
+mysql_connect('localhost', 'root', 'List7Arch7tailor') or die("I couldn't connect to your database, please make sure your info is correct!");
+mysql_select_db('testdeco3801') or die("I couldn't find the database table ($table) make sure it's spelt right!");
